cbb72080fb16ce0606e2c30ddfd77663fd7309de
pratikshabathe <pratikshabathe11@gmail.com>
2023-02-01 20:44:15 +0530

    Added: ability to create linked list

--- LinkedListProblem.java
+++ LinkedListProblem.java
@@ -12,56 +12,49 @@ public class LinkedListProblem {
 	 Node head;
 	    Node tail;
 
-	    public Node  push(int data) {
+	    public  void push(int data) {
 	        Node newNode = new Node(data);
 	        if (head == null) {
 	            head = newNode;
-	          //  tail = newNode;
+	            tail = newNode;
 	        } else {
-	            Node temp = head;// 56=>30=>70
-	            this.head = newNode;
-	            newNode.next = temp;
+	        	newNode.next = head;																																		
+	        	head = newNode;
 	        }
-	        return newNode;
+	        
+	    }
+	    
+	    public void add(int data) {
+	        Node newNode = new Node(data);
+	        if (head == null) {
+	            head = newNode;
+	            tail = newNode;
+	        } else {
+	        	tail.next = newNode;
+	        	tail = newNode;
+	        }
+	        
 	    }
 
 	    public void print() {
-	        if (head == null) {
-	            System.out.println("Linked List is Empty");
-	        } else {
 	            Node temp = head;
 	            while (temp != null) {
 	                System.out.print(temp.data+ " ");
 	                temp = temp.next;
 	            }
 	        }
-	    }
 
-	    public void append(int data) { // 56=> 30=> 70
-	        Node newNode = new Node(data);
-	        if (head == null) {
-	            head = newNode;
-	            tail = newNode;
-	        } else {
-	            this.tail.next = newNode;
-	            tail = newNode;
-	        }
-	    }
-
-	    public void insertInBetween(Node previousNode ,Node newNode){
-	        Node tempNode = previousNode.next;
-	        previousNode.next = newNode;
-	        newNode.next = tempNode;
-	    }
+	    
+	    
 	    public static void main(String[] args) {
 	        System.out.println("Welcome to linkedList DataStructure Program");
 	        
 	        Scanner scanner = new Scanner(System.in);
 	        LinkedList linkedList = new LinkedList();
-  	        linkedList.push(70);
-  	        linkedList.push(30);
   	        linkedList.push(56);
+  	        linkedList.push(30);
+  	        linkedList.push(70);
   	     
-  	      System.out.printf(" add the data at the start: " +linkedList);
+  	      System.out.printf(" add the data at the end: " +linkedList);
 	    }
 }
